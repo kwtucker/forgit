2084be849562cf1f20b49ace8c30769f276aa2b9
Kevin Tucker <kwtucker4@gmail.com>
2016-09-27 12:36:09 -0400

    Prompt and validate input

--- lib/init.go
+++ lib/init.go
@@ -1,1 +1,52 @@
 package lib
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+	// "strings"
+)
+
+func isPath(p string) bool {
+	switch string(p[0]) {
+	case "/", ".", "~":
+		return true
+	}
+	return false
+}
+
+// Init ...
+func Init() {
+	var (
+		err    error
+		reader *bufio.Reader
+		path   string
+	)
+	currentDir, err := os.Getwd()
+	if err != nil {
+		fmt.Println(err)
+	}
+	reader = bufio.NewReader(os.Stdin)
+	fmt.Println("Your Current Absolute Path is ->", currentDir)
+	fmt.Println("Path can be a relative or absolute path.")
+	fmt.Println("-=-=-")
+	fmt.Print("Path to your Forgit Directory: ")
+
+	path, err = reader.ReadString('\n')
+	if err != nil {
+		fmt.Println(err)
+	}
+
+	valid := isPath(path)
+	if !valid {
+		fmt.Println()
+		fmt.Println()
+		fmt.Println("NOT VALID PATH. Must start with '.' , '/' , '~'")
+		fmt.Println("Example: ~/Desktop/Forgit/")
+		fmt.Println("Suggest going to the Forgit Directory and running pwd command to get its path")
+		fmt.Println("[ or ]")
+		fmt.Println("Run Again --> fgt init")
+		return
+	}
+	fmt.Println(path)
+}
