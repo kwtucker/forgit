6f533be204217b496e274f600626164513e60c39
Kevin Tucker <kwtucker4@gmail.com>
2016-09-30 16:35:23 -0400

    added a function to get all repos in Forgit directory  Added some variable initializers Worked around some logic checking values from command Added a conditional to check if setting struct is empty that will build a setting for that session. if user writes fgt start -c 5 -p 5. it will build a setting struct for that session

--- lib/start.go
+++ lib/start.go
@@ -5,6 +5,8 @@ import (
 	"fmt"
 	"github.com/urfave/cli"
 	"io/ioutil"
+	"log"
+	"net/http"
 	"os"
 	osuser "os/user"
 )
@@ -18,6 +20,33 @@ func settingGroupsCheck(groupName string, dataUser User) (Setting, bool) {
 	return dataUser.Settings[0], false
 }
 
+// ForgitDirReposNames ...
+func ForgitDirReposNames(path string) []string {
+	var forgitDirReposNameSlice []string
+	files, err := ioutil.ReadDir(path)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	for _, file := range files {
+		if file.IsDir() {
+			forgitDirReposNameSlice = append(forgitDirReposNameSlice, file.Name())
+		}
+	}
+	return forgitDirReposNameSlice
+}
+
+// InternetCheck Checks if internet exists
+func InternetCheck() bool {
+	_, err := http.Get("http://google.com/")
+	if err != nil {
+		fmt.Println(err)
+		fmt.Println("No internet")
+		return false
+	}
+	return true
+}
+
 // Start ...
 func Start(c *cli.Context) {
 
@@ -25,7 +54,15 @@ func Start(c *cli.Context) {
 		commit, push int
 		group        string
 		dataUser     []User
+		settingObj   Setting
+		settingRepos []SettingRepo
+		settingRepo  SettingRepo
+		setExist     bool
 	)
+
+	// Internet Check
+	internetConnection := InternetCheck()
+
 	// Check if .forgitConf.json exists
 	homeDir, err := osuser.Current()
 	if err != nil {
@@ -33,6 +70,7 @@ func Start(c *cli.Context) {
 		os.Exit(1)
 	}
 
+	// Check if config file exists.
 	if _, err = os.Stat(homeDir.HomeDir + "/.forgitConf.json"); os.IsNotExist(err) {
 		fmt.Println()
 		fmt.Println("* Haven't started yet!")
@@ -52,49 +90,38 @@ func Start(c *cli.Context) {
 
 	fmt.Println("This session will have the following settings:")
 
-	// if commit set value
-	if c.IsSet("commit") && c.Int("commit") != 0 {
-		fmt.Println("  - Commit:", c.Int("commit"))
-		commit = c.Int("commit")
-	} else if c.IsSet("commit") == false {
-		commit = -1
-	} else {
-		fmt.Println("  - Default Push: 5")
-		commit = 10
-	}
-
-	// if push set value
-	if c.IsSet("push") && c.Int("push") != 0 {
-		fmt.Println("  - Push:", c.Int("push"))
-		push = c.Int("push")
-	} else if c.IsSet("push") == false {
-		push = -1
-	} else {
-		fmt.Println("  - Default Push: 60")
-		push = 60
-	}
-
 	// If group exists grab group name and set the values
 	if c.Args().First() != "" {
 		fmt.Println("  - Group:", c.Args().First())
 		group = c.Args().First()
-		// settings set on
 
-		settingObj, setExist := settingGroupsCheck(group, dataUser[0])
-		if !setExist {
+		settingObj, setExist = settingGroupsCheck(group, dataUser[0])
+		if setExist == false {
 			fmt.Println()
 			fmt.Println("* Did Not Start!")
 			fmt.Println("* Setting Workspace group does not exist.")
 			fmt.Println()
 			return
 		}
-		// set group vars
-		commit = 2
-		push = 2
-
 	} else {
 		fmt.Println("  - Group Not Set")
 		group = "-1"
+	}
+
+	// if commit set value
+	if c.IsSet("commit") && c.Int("commit") != 0 && group == "-1" {
+		fmt.Println("  - Commit:", c.Int("commit"))
+		commit = c.Int("commit")
+	} else {
+		commit = -1
+	}
+
+	// if push set value
+	if c.IsSet("push") && c.Int("push") != 0 && group == "-1" {
+		fmt.Println("  - Push:", c.Int("push"))
+		push = c.Int("push")
+	} else {
+		push = -1
 	}
 
 	fmt.Println(" commit ", commit)
@@ -109,15 +136,94 @@ func Start(c *cli.Context) {
 	*/
 
 	// git byte array from MarshalIndent
-	configDataBytes, err := json.MarshalIndent(dataUser, "", "    ")
+	// configDataBytes, err := json.MarshalIndent(dataUser, "", "    ")
+	_, err = json.MarshalIndent(dataUser, "", "    ")
 	if err != nil {
 		fmt.Println(err.Error())
 		os.Exit(1)
 	}
 
-	fmt.Println(dataUser[0].GithubID)
-	fmt.Println(string(configDataBytes))
+	// if no push,commit, or group set. Grab setting with status of 1
+	if c.IsSet("push") == false && c.IsSet("commit") == false && group == "-1" {
+		for s := range dataUser[0].Settings {
+			if dataUser[0].Settings[s].Status == 1 {
+				settingObj = dataUser[0].Settings[s]
+			}
+		}
+	}
 
+	// Send setting repos with status 1 to a slice.
+	for r := range settingObj.Repos {
+		if settingObj.Repos[r].Status == 1 {
+			settingRepos = append(settingRepos, settingObj.Repos[r])
+		}
+	}
+
+	// If push or commit set and no group build a struct.
+	// This only lasts per session
+	if settingObj.Name == "" {
+		var (
+			setPush, setCommit map[string]int
+		)
+
+		if push != -1 {
+			setPush = map[string]int{
+				"Status":  1,
+				"TimeMin": push,
+			}
+		} else {
+			setPush = map[string]int{
+				"Status":  0,
+				"TimeMin": push,
+			}
+		}
+
+		if commit != -1 {
+			setCommit = map[string]int{
+				"Status":  1,
+				"TimeMin": commit,
+			}
+		} else {
+			setCommit = map[string]int{
+				"Status":  0,
+				"TimeMin": commit,
+			}
+		}
+
+		// Set all the repos in the forgit directory to status 1 on.
+		repoNames := ForgitDirReposNames(dataUser[0].ForgitPath + "/Forgit/")
+		for r := range repoNames {
+			settingRepo = SettingRepo{
+				GithubRepoID: r,
+				Name:         repoNames[r],
+				Status:       1,
+			}
+			settingRepos = append(settingRepos, settingRepo)
+		}
+
+		// Struct build for the session setting
+		settingObj = Setting{
+			SettingID: 0,
+			Name:      "fgtDefault",
+			Status:    1,
+			SettingNotifications: map[string]int{
+				"Status":   1,
+				"OnError":  1,
+				"OnCommit": 1,
+				"OnPush":   1,
+			},
+			SettingAddPullCommit: setCommit,
+			SettingPush:          setPush,
+			Repos:                settingRepos,
+		}
+	}
+
+	fmt.Println("dataUser ID ->", dataUser[0].GithubID)
+	// fmt.Println(string(configDataBytes))
+	fmt.Println("settingObj Name ->", settingObj.Name)
+	fmt.Println("settingObj commit time ->", settingObj.SettingAddPullCommit["TimeMin"])
+	fmt.Println("settingObj repo index 0 name ->", settingObj.Repos)
+	fmt.Println("internetConnection ->", internetConnection)
 	// Grab all repo names from config with status 1
 
 	// Set Forgit path
