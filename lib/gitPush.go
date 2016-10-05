4f9c9d2d03c5d66d538c999fd1c3453bcb3f6e1f
Kevin Tucker <kwtucker4@gmail.com>
2016-10-05 15:50:03 -0400

    push function stub out for testing

--- lib/gitPush.go
+++ lib/gitPush.go
@@ -1,1 +1,18 @@
 package lib
+
+import (
+	"fmt"
+	// "time"
+)
+
+//GitPush is simply a git push command
+func GitPush(ptime int, repos []SettingRepo) {
+
+	for {
+		// a delay in the for loop
+		Ticker(ptime)
+		// Where the push code is going
+		fmt.Println("push")
+	}
+
+}
