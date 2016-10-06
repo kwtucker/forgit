1f0bf389a01b163e694656cb38dcb734518d38b5
Kevin Tucker <kwtucker4@gmail.com>
2016-10-06 17:41:04 -0400

    git add command

--- lib/gitAdd.go
+++ lib/gitAdd.go
@@ -1,1 +1,18 @@
 package lib
+
+import (
+	// "fmt"
+	// "log"
+	// "os"
+	"os/exec"
+	// "strings"
+	// "sync"
+)
+
+func GitAdd(file string) {
+
+	// Git status command
+	gsArgs := []string{"add", file}
+	gitStatus := exec.Command("git", gsArgs...)
+	gitStatus.Run()
+}
