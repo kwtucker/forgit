1b211328ef546deb6a01442492026ebcefee5c3b
Kevin Tucker <kwtucker4@gmail.com>
2016-10-06 15:45:57 -0400

    added a status function

--- lib/gitStatus.go
+++ lib/gitStatus.go
@@ -1,71 +1,43 @@
 package lib
 
 import (
-	"bufio"
-	"fmt"
 	"log"
 	"os"
 	"os/exec"
 	"strings"
-	"sync"
 )
 
-func GitStatus(committime int, path string, repos []SettingRepo) {
+// Status gets the git status of the repos directory
+func Status(p string) ([]string, error) {
+	var (
+		err      error
+		fileName string
+		modfiles []string
+	)
 
-	for {
-		var (
-			gitStatus *exec.Cmd
-			fileName  string
-		)
-
-		// Ticker to make for loop wait tell push time
-		Ticker(committime)
-
-		// making a type
-		reposMap := make(map[string][]string)
-
-		for r := range repos {
-			// creating a new wait group and adding 1 goroutine to it.
-			var ww sync.WaitGroup
-			ww.Add(1)
-
-			var err error
-			err = os.Chdir(path + repos[r].Name)
-			// Git status command
-			gsArgs := []string{"status", "-s"}
-			gitStatus = exec.Command("git", gsArgs...)
-
-			// Grab the stdout of git status
-			gsOut, err := gitStatus.StdoutPipe()
-			if err != nil {
-				log.Println(err)
-			}
-
-			// Create a new scanner to parse the gsOut
-			scanner := bufio.NewScanner(gsOut)
-			// go routine with WaitGroup hold the function before end.
-			// Scan each line, get the text, split it by the white space.
-			// append the text the filename
-			go func(n map[string][]string) {
-				defer ww.Done()
-				for scanner.Scan() {
-					line := scanner.Text()
-					lineArr := strings.Split(line, " ")
-					if len(lineArr[1]) > 2 {
-						fileName = lineArr[1]
-					} else {
-						fileName = lineArr[2]
-					}
-					var modfiles []string
-					modfiles = append(modfiles, fileName)
-					n[repos[r].Name] = modfiles
-				}
-			}(reposMap) // pass in map to go routine
-
-			gitStatus.Start()
-			ww.Wait()
-		}
-		fmt.Println("Modified Files", reposMap)
+	// Change directory to the path passed in
+	err = os.Chdir(p)
+	if err != nil {
+		log.Println(err)
 	}
 
+	// Git status command
+	gsArgs := []string{"status", "-s"}
+	gitStatus := exec.Command("git", gsArgs...)
+
+	// Get stdout and trim the empty last index
+	fileStatus, err := gitStatus.Output()
+	fsSplit := strings.Split(string(fileStatus), "\n")
+	lineArr := fsSplit[0 : len(fsSplit)-1]
+
+	for i := range lineArr {
+		splitIndex := strings.Split(lineArr[i], " ")
+		if len(splitIndex[1]) >= 2 {
+			fileName = splitIndex[1]
+		} else {
+			fileName = splitIndex[2]
+		}
+		modfiles = append(modfiles, fileName)
+	}
+	return modfiles, err
 }
