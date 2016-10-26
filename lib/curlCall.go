e680f65c85d6f8c8d30bd33ff6ad6892d3acf3fa
Kevin Tucker <kwtucker4@gmail.com>
2016-10-26 17:19:27 -0400

    set curl call back to the master server

--- lib/curlCall.go
+++ lib/curlCall.go
@@ -9,12 +9,12 @@ import (
 func Curlforgit(init string, fid string) ([]byte, error) {
 	// Curl call that I am hooking up to forgit server later
 	if init == "init" {
-		resp, err := http.Get("http://forgit-stage.whalebyte.com/api/users/" + fid + "/" + init)
+		resp, err := http.Get("http://forgit.whalebyte.com/api/users/" + fid + "/" + init)
 		defer resp.Body.Close()
 		databytes, err := ioutil.ReadAll(resp.Body)
 		return databytes, err
 	}
-	resp, err := http.Get("http://forgit-stage.whalebyte.com/api/users/" + fid + "/no")
+	resp, err := http.Get("http://forgit.whalebyte.com/api/users/" + fid + "/no")
 	defer resp.Body.Close()
 	databytes, err := ioutil.ReadAll(resp.Body)
 	return databytes, err
