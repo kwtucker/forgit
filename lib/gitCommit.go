f60261f312bf47ad2636f6a65a7f4890d4ba59f2
Kevin Tucker <kwtucker4@gmail.com>
2016-10-31 15:59:04 -0400

    made a simple default commit message call Forgit Backup #50

--- lib/gitCommit.go
+++ lib/gitCommit.go
@@ -8,6 +8,11 @@ import (
 // GitCommit command.
 func GitCommit(message string, wg *sync.WaitGroup, notifyme int, notifymeError int) {
 	defer wg.Done()
+
+	// For a default message in case user doesn't enter message in code.
+	if message == " " || message == "" {
+		message = "Forgit Backup"
+	}
 	// Git commit command
 	args := []string{"commit", "-m", message}
 	cmd := exec.Command("git", args...)
