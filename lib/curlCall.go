adf1cfef941aa6c72894a336e5cd9f6567e1d0ef
Kevin Tucker <kwtucker4@gmail.com>
2016-11-12 10:33:49 -0500

    Hotfix with comments cli output

--- lib/curlCall.go
+++ lib/curlCall.go
@@ -7,13 +7,14 @@ import (
 
 // Curlforgit ...
 func Curlforgit(init string, fid string) ([]byte, error) {
-	// Curl call that I am hooking up to forgit server later
+	// Calls forgitWeb and expects a full user object back.
 	if init == "init" {
 		resp, err := http.Get("http://forgit.whalebyte.com/api/users/" + fid + "/" + init)
 		defer resp.Body.Close()
 		databytes, err := ioutil.ReadAll(resp.Body)
 		return databytes, err
 	}
+	// Only returns full user object if it has been updated.
 	resp, err := http.Get("http://forgit.whalebyte.com/api/users/" + fid + "/no")
 	defer resp.Body.Close()
 	databytes, err := ioutil.ReadAll(resp.Body)
